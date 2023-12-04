3fce710a251d0ac45ba0b2f236adf45484a03880
rhythmdev <rhythmbiswas@gmail.com>
2023-12-04 14:00:59 +0600

    sign up and sign in form basic setup done and context api basix setup done

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,8 +1,58 @@
-
+import { Link } from "react-router-dom";
+import logo from '../../assets/logo.svg'
+import { Button } from "flowbite-react";
+import { FcGoogle } from "react-icons/fc";
 const SignIn = () => {
+
+    const handelSignIn = e => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(email, password);
+    }
+
     return (
         <div>
-               <h2 className="text-2xl text-purple-400">Sign In</h2>
+            <section className="bg-gray-100 dark:bg-gray-900 rounded py-8">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+                    <Link to={'/'} className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <img className="w-full h-10" src={logo} alt="logo" />
+                        <span className='font-lobster bg-gradient-to-r from-gradient-start to-gradient-end  text-transparent bg-clip-text'>EliteAutos</span>
+                    </Link>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gradient-end md:text-2xl dark:text-white">
+                                Sign in to your account
+                            </h1>
+                            <form onSubmit={handelSignIn} className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                </div>
+
+                                <button type="submit" className="w-full text-white bg-gradient-to-r from-gradient-start to-gradient-end hover:focus:ring-4 focus:outline-none focus:ring-gradient-start font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gradient-end dark:hover:bg-gradient-start dark:focus:ring-gradient-start">Create an account</button>
+                                <span className="flex items-center">
+                                    <span className="h-px flex-1 bg-gray-400"></span>
+                                    <span className="shrink-0 px-6 font-medium">Or Sign in with</span>
+                                    <span className="h-px flex-1 bg-gray-400"></span>
+                                </span>
+                                <Button className='bg-gradient-to-r from-gradient-start to-gradient-end border-none mx-auto'>
+                                    Google
+                                    <FcGoogle className="ml-2 h-5 w-5" />
+                                </Button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don’t have an account yet? <Link to='/signUp' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign Up</Link>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,8 +1,77 @@
-
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg'
+import { FcGoogle } from "react-icons/fc";
+import { Button } from 'flowbite-react';
 const SignUp = () => {
+
+    const handelSignUp = e => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const email = form.get('email');
+        const photoURl = form.get('photoUrl');
+        const password = form.get('password');
+        console.log(name, email, photoURl, password);
+
+    }
+
     return (
         <div>
+            <section className="bg-gray-100 dark:bg-gray-900 py-8 rounded">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  lg:py-0">
+                    <Link to={'/'} className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <img className="w-full h-10" src={logo} alt="logo" />
+                        <span className='font-lobster bg-gradient-to-r from-gradient-start to-gradient-end  text-transparent bg-clip-text'>EliteAutos</span>
+                    </Link>
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gradient-end md:text-2xl dark:text-white">
+                                Create and account
+                            </h1>
+                            <form onSubmit={handelSignUp} className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label htmlFor="name" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Your Name</label>
+                                    <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ryan Dahl" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Your Email</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ryandahl@gmail.com" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="photUrl" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Your Photo Url</label>
+                                    <input type="text" name="photoUrl" id="photoUrl" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://i.ibb.co/CbB0XmF/profile-pic.jpg" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                </div>
 
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                                    </div>
+                                </div>
+                                <button type="submit" className="w-full text-white bg-gradient-to-r from-gradient-start to-gradient-end hover:focus:ring-4 focus:outline-none focus:ring-gradient-start font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gradient-end dark:hover:bg-gradient-start dark:focus:ring-gradient-start">Create an account</button>
+                                <span className="flex items-center">
+                                    <span className="h-px flex-1 bg-gray-400"></span>
+                                    <span className="shrink-0 px-6 font-medium">Or Sign up with</span>
+                                    <span className="h-px flex-1 bg-gray-400"></span>
+                                </span>
+                                <Button className='bg-gradient-to-r from-gradient-start to-gradient-end border-none mx-auto'>
+                                    Google
+                                    <FcGoogle className="ml-2 h-5 w-5" />
+                                </Button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Already have an account? <Link to='/signIn' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In</Link>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
